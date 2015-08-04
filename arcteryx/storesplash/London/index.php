c255d2bd01de083b6163d58e330664f15ef49171
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2015-08-04 11:57:59 -0700

    Moved London Store page to its own folder, created NYC store splash

--- arcteryx/storesplash/London/index.php
+++ arcteryx/storesplash/London/index.php
@@ -5,18 +5,9 @@
     <link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/a1fce17f-2d2d-4ce7-8a00-0f2382646d8b.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
 </head>
 <body>
-
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KRS44Z"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KRS44Z');</script>
-<!-- End Google Tag Manager -->
 
 <div id="fade"></div>
 
