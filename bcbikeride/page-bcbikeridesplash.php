6b301f2099ddc29e7d9b53e509a6e66c8f915b98
ckoMich <ck.oconnell@gmail.com>
2017-02-10 01:32:21 -0800

    BC Bike - Added GA script to php template

--- bcbikeride/page-bcbikeridesplash.php
+++ bcbikeride/page-bcbikeridesplash.php
@@ -18,6 +18,16 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
 	<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-279258-7', 'auto');
+	  ga('send', 'pageview');
+	</script>
 
 </head>	
 	
