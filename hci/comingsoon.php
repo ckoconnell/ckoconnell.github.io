3a8be1a4e3097721ea94cbc7b8fe628877cab91b
ckoconnell <ck.oconnell@gmail.com>
2019-05-08 19:56:19 -0700

    HCI WP Coming Soon template

---
+++ hci/comingsoon.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+Template Name: Coming Soon
+*/
+
+?>
+
+<!-- <!DOCTYPE html> -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900" rel="stylesheet">
+	<title>He Changed It.</title>
+  <script type="text/javascript" src="http://hechangedit.com/wp-includes/js/jquery/jquery.js?ver=1.12.4"></script>
+  <style>
+  html,body{padding:0;margin:0;vertical-align:top;position:relative;display:block;color:#fff;font-family:"Nunito Sans",sans-serif;background-color:#232323}div{box-sizing:border-box}header{z-index:10;position:absolute;width:100%;height:100px}header ul{list-style:none;margin:0 auto;margin-top:1.5rem;width:100%;text-align:center;padding-left:0;margin-left:0}header ul li{list-style:none;padding-left:0;margin-left:0}header ul li img{width:70px}section{width:100%;height:auto;margin:0}section .gridWrap{margin:0 auto;display:block;position:relative;width:100%;max-width:1030px;padding:6rem 2rem}.hero{padding:5rem 0}h1{font-weight:100;color:transparent;font-size:4rem;line-height:3.5rem;text-align:center;margin:0 auto;margin-top:3rem;margin-bottom:6rem;-webkit-text-stroke-width:1px;-webkit-text-stroke-color:#C8D1D3}h3{font-weight:200;color:#87BCBF;font-size:2.5rem;text-align:center;margin:0 auto;margin-top:3rem}h5{font-weight:300;color:#C8D1D3;font-size:.85rem;text-align:center;margin:0 auto;margin-top:0rem;margin-bottom:2rem;width:auto;max-width:600px;text-transform:uppercase}p{font-weight:100;color:#C8D1D3;font-size:1rem;text-align:center;margin:0 auto;margin-top:1rem;margin-bottom:2rem;width:auto;max-width:400px}a{text-decoration:none;color:#87BCBF}button{margin:0 auto;padding:.75rem 1.25rem;color:#87BCBF;background:none;text-align:center;border:2px solid #87BCBF;font-weight:300;font-size:1rem;text-transform:uppercase;display:block;transition:all .5s;cursor:pointer}button:hover{color:#1B1C20;background:#87BCBF;transition:all .8s}#intro{background-image:url("http://hechangedit.com/wp-content/uploads/2019/05/imgHero01.jpg");background-size:cover;background-position:75% bottom}#intro .gridWrap p{line-height:1.5rem}#about{background-color:#F0F3F4;background-size:cover;background-position:center center;color:#6E8CA0}#about ul{text-align:center}#about ul li{display:block;text-align:center;vertical-align:top;margin:2rem 1rem;color:#6E8CA0;font-weight:400;font-size:1.5rem;line-height:2rem}#contact{background-image:url("http://hechangedit.com/wp-content/uploads/2019/05/imgHero02.jpg");background-size:cover;background-position:center center;padding:8rem 0}#contact h5{font-size:1.5rem;text-transform:none}#contact p{font-size:1.25rem;font-weight:400;color:#C8D1D3;margin-bottom:2rem}#contact .formWrap{width:100%;margin:0 auto;text-align:center}#contact input{border:2px solid #87BCBF;background:none;color:#87BCBF;padding:.75rem 1.25rem;display:inline-block;font-weight:100;font-size:1rem;text-transform:uppercase;margin-right:1rem;margin-bottom:1rem}#contact button{display:inline-block}#contactV2{background-color:#2f2f32;padding:8rem 0}#contactV2 h5{font-size:1.5rem;text-transform:none}#contactV2 p{font-size:1.25rem;font-weight:400;color:#C8D1D3;margin-bottom:2rem}#contactV2 .formWrap{width:100%;margin:0 auto;text-align:center}#contactV2 input{border:2px solid #87BCBF;background:none;color:#87BCBF;padding:.75rem 1.25rem;display:inline-block;font-weight:100;font-size:1rem;text-transform:uppercase;margin-right:1rem;margin-bottom:1rem}#contactV2 button{display:inline-block}
+
+  </style>
+
+</head>
+<body>
+	<header>
+		<ul>
+			<li><img src="http://hechangedit.com/wp-content/uploads/2019/05/logo.png" /></li>
+		</ul>
+	</header>
+  <section class="hero" id="intro">
+    <div class="gridWrap">
+			<h1>COMING SOON</h1>
+			<h3>Welcome to he.</h3>
+			<h5>A men’s only platform launching June 2019.</h5>
+      <p>A mentor exchange peer to peer community.<br/>Add to someone’s life.<br/> Take what you need to learn.<br/> Judgement free.<br/> Anonymous if you want.</p>
+			<a href="#contactV2"><button>Sign Up</button></a>
+    </div>
+  </section>
+	<section class="form" id="contactV2">
+		<div class="gridWrap">
+			<h5>Get social with us on <a href="https://www.facebook.com/groups/hechangedit/">Facebook</a> for now.</h5>
+			<p>Fill out the form below to find out how you can help us build this.</p>
+			<div class="formWrap">
+
+
+        <?php echo do_shortcode("[mc4wp_form id=169]"); ?>
+
+				<!-- <form>
+					<input type="text" value="Name">
+					<input type="text" value="Email Address">
+					<button type="submit">Sign Up</button>
+				</form> -->
+			</div>
+		</div>
+	</section>
+  <footer></footer>
+</body>
+</html>
