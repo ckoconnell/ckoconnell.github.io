64d9377d19f3add19401a7087550f0e62a71914c
ckoconnell <ck.oconnell@gmail.com>
2019-07-09 15:41:40 -0700

    HCI & HM Avy Test Build

---
+++ hci/p1.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+Template Name: Phase One Home
+*/
+
+?>
+
+<!-- <!DOCTYPE html> -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,600,700,800,900" rel="stylesheet">
+	<title>He Changed It.</title>
+  <script type="text/javascript" src="http://hechangedit.com/wp-includes/js/jquery/jquery.js?ver=1.12.4"></script>
+  <style>
+html,body{padding:0;margin:0;vertical-align:top;position:relative;display:block;color:#fff;font-family:"Nunito Sans",sans-serif;background-color:#232323}div{box-sizing:border-box}header{z-index:10;position:absolute;width:100%;height:100px}header ul{list-style:none;margin:0 auto;margin-top:2.5rem;width:100%;text-align:center;padding-left:0;margin-left:0}header ul li{list-style:none;padding-left:0;margin-left:0}header ul li img{width:70px}footer{padding:2rem 0 0rem}footer h5{color:#6E8CA0;font-size:.75rem}section{width:100%;height:auto;margin:0}section .gridWrap{margin:0 auto;display:block;position:relative;width:100%;max-width:1030px;padding:6rem 2rem}.hero{padding:8rem 0}h1{font-weight:100;color:transparent;font-size:4rem;line-height:3.5rem;text-align:center;margin:0 auto;margin-top:3rem;margin-bottom:6rem;-webkit-text-stroke-width:1px;-webkit-text-stroke-color:#C8D1D3}h3{font-weight:600;color:#87BCBF;font-size:1.5rem;text-align:center;margin:0 auto;margin-top:3rem;text-transform:uppercase}h5{font-weight:300;color:#C8D1D3;font-size:.85rem;text-align:center;margin:0 auto;margin-top:.5rem;margin-bottom:2rem;width:auto;max-width:600px}p{font-weight:400;color:#C8D1D3;font-size:1rem;text-align:left;margin:0 auto;margin-top:1rem;margin-bottom:2rem;width:auto;max-width:400px}a{text-decoration:none;color:#87BCBF}button{margin:0 auto;padding:.75rem 1.25rem;color:#87BCBF;background:none;text-align:center;border:2px solid #87BCBF;font-weight:300;font-size:1rem;text-transform:uppercase;display:block;transition:all .5s;cursor:pointer}button:hover{color:#1B1C20;background:#87BCBF;transition:all .8s}#intro{background-image:url("http://hechangedit.com/wp-content/uploads/2019/05/imgHero01.jpg");background-size:cover;background-position:75% bottom}#intro .gridWrap p{line-height:1.5rem}#about{background-color:#F0F3F4;background-size:cover;background-position:center center;color:#6E8CA0}#about ul{text-align:center}#about ul li{display:block;text-align:center;vertical-align:top;margin:2rem 1rem;color:#6E8CA0;font-weight:400;font-size:1.5rem;line-height:2rem}#about h3{color:#6E8CA0;font-weight:600}#about p{color:#1B1C20}#about a{font-weight:600;color:#6E8CA0}#contact{background-color:#2f2f32;background-image:url("http://hechangedit.com/wp-content/uploads/2019/05/imgHero02.jpg");background-size:cover;padding:8rem 0}#contact h5{font-size:1.5rem;text-transform:none}#contact .formWrap{width:100%;margin:0 auto;text-align:center}#contact input{border:2px solid #87BCBF;background:none;color:#87BCBF;padding:.75rem 1.25rem;display:block;font-weight:100;font-size:1rem;text-transform:uppercase;margin:0 auto;margin-bottom:1rem}#contact button{display:block}#investors{background-color:#2f2f32;background-size:cover;background-position:center center;color:#6E8CA0}#investors ul{text-align:center}#investors ul li{display:block;text-align:center;vertical-align:top;margin:2rem 1rem;color:#6E8CA0;font-weight:400;font-size:1.5rem;line-height:2rem}#investors a{font-weight:600;color:#87BCBF}
+  </style>
+
+</head>
+<body>
+	<header>
+		<ul>
+			<li><img src="http://hechangedit.com/wp-content/uploads/2019/05/logo.png" /></li>
+		</ul>
+	</header>
+  <section class="hero" id="intro">
+    <div class="gridWrap">
+			<h3>Welcome to he.</h3>
+			<h5>A men’s only platform to just be you</h5>
+			<a href="#contact"><button>Sign Up</button></a>
+    </div>
+  </section>
+	<section class="" id="about">
+    <div class="gridWrap">
+			<h3>About</h3>
+			<p>A judgement free peer to peer exchange platform that you can access personally, or through an anonymous toggle. Use your story to help someone through a rough time: a job change, family issues, thoughts, loneliness. In return, engage other’s expertise in areas that interest you: Personal development, entrepreneurship, relationships…anything really that can help you advance in life.  </p>
+			<p>This is your space. Become part of the community so you can be part of the solution. Join our <a href="https://www.facebook.com/groups/hechangedit/" target="_blank">Facebook Group</a> or <a href="contact">Sign Up</a> to stay updated and become a beta tester for the he. App being released this summer. </p>
+			<p>he. Is for you. A place to go to transfer the knowledge you have and receive the knowledge you want. </p>
+    </div>
+  </section>
+	<section class="form" id="contact">
+		<div class="gridWrap">
+			<p>Fill out the form below to find out how you can help us build this.</p>
+			<div class="formWrap">
+
+
+        <?php echo do_shortcode("[mc4wp_form id=169]"); ?>
+
+				<!-- <form>
+					<input type="text" value="Name">
+					<input type="text" value="Email Address">
+					<button type="submit">Sign Up</button>
+				</form> -->
+			</div>
+		</div>
+	</section>
+	<section class="" id="investors">
+		<div class="gridWrap">
+			<h3>Investors</h3>
+			<p>HEchangedit is leveraging technology to disrupt the status quo by developing gender-based consciousness to today’s burgeoning concerns for men, including today’s social climate, the rising costs of health, fair legislation regarding divorce and family, and more.  </p>
+			<p>Interested in doubling your bottom line by investing in an organization focused on making positive social impact in an underserved market with multiple revenue streams? </p>
+			<p>Please connect with us at <a href="mailto:info@hechangedit.com">info@hechangedit.com</a> to learn more now. </p>
+		</div>
+	</section>
+
+  <footer>
+		<h5>&copy; copyright 2019 hechangedit. </h5>
+	</footer>
+</body>
+</html>
