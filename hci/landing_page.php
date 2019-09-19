3ac201768142e624a2175a9e510834c2cc05957c
ckoconnell <ck.oconnell@gmail.com>
2019-09-18 18:50:57 -0700

    HCI  Facebook Pixel Code Update

--- hci/landing_page.php
+++ hci/landing_page.php
@@ -5,7 +5,7 @@ Template Name: Landing Page
 global $wp_query;
 global $qode_options_proya;
 $id = $wp_query->get_queried_object_id();
-$sidebar = get_post_meta($id, "qode_show-sidebar", true);  
+$sidebar = get_post_meta($id, "qode_show-sidebar", true);
 
 $enable_page_comments = false;
 if(get_post_meta($id, "qode_enable-page-comments", true) == 'yes') {
@@ -59,7 +59,23 @@ else { $paged = 1; }
         <![endif]-->
 
         <?php wp_head(); ?>
-
+				<!-- Facebook Pixel Code -->
+					<script>
+					  !function(f,b,e,v,n,t,s)
+					  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					  n.queue=[];t=b.createElement(e);t.async=!0;
+					  t.src=v;s=b.getElementsByTagName(e)[0];
+					  s.parentNode.insertBefore(t,s)}(window, document,'script',
+					  'https://connect.facebook.net/en_US/fbevents.js');
+					  fbq('init', '354449031913069');
+					  fbq('track', 'PageView');
+					</script>
+					<noscript><img height="1" width="1" style="display:none"
+					  src="https://www.facebook.com/tr?id=354449031913069&ev=PageView&noscript=1"
+					/></noscript>
+					<!-- End Facebook Pixel Code -->
     </head>
 
 <body <?php body_class(); ?>>
